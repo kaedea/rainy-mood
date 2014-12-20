552dca2f9bc28e0f435071c91b1ba8a6af5c8006
Administrator <Administrator@Kaede-Nanami>
2014-12-20 18:17:02 +0800

    first

---
+++ src/com/kaede/rainymood/MainActivity.java
@@ -0,0 +1,66 @@
+package com.kaede.rainymood;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) this.findViewById(R.id.main_viewPager);
+        MainFragmentStateAdapter mainFragmentStateAdapter = new MainFragmentStateAdapter(getSupportFragmentManager());
+		viewPager.setAdapter(mainFragmentStateAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static final class MainFragmentStateAdapter extends FragmentStatePagerAdapter {
+    	public MainFragmentStateAdapter(FragmentManager fm) {
+    		super(fm);
+    	}
+    	
+    	@Override
+    	public int getCount() {
+    		// TODO Auto-generated method stub
+    		return 5;
+    	}
+    	
+    	@Override
+    	public Fragment getItem(int arg0) {
+    		// TODO Auto-generated method stub
+    		return new MainFragment(arg0);
+    	}
+    }
+}
